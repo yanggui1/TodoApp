4b89cb8a43ac5efd46f658faad56326a8d9be062
yanggui2 <yanggui2@lenovo.com>
2019-03-22 15:32:53 +0800

    css style

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
     // 获取数据
     app.get("/todo", function(req,res) {
-        res.send("您所访问的页面地址是: "+req.url);
+        res.render("todo");
     })
 
     // 传递数据
