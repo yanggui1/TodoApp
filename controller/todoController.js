d53835a86b486f32eeeb14137a529665bfbf107c
yanggui2 <yanggui2@lenovo.com>
2019-03-22 17:50:59 +0800

    commit post

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,7 +1,17 @@
+var bodyParser = require("body-parser");
+// 对数据进行解析
+var urlencoderParser = bodyP
+
+var data = [
+    {item: "欢迎大家来到双核浏览器！"},
+    {item: "双核浏览器极速体验"},
+    {item: "主要是双核极速"}
+]
+
 module.exports = function (app) {
     // 获取数据
     app.get("/todo", function(req,res) {
-        res.render("todo");
+        res.render("todo",{todos:data});
     })
 
     // 传递数据
